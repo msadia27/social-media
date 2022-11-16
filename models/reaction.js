21a912609aee0f40bfe46129f7139548467a9903
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-15 23:47:59 -0500

    working on models folder

---
+++ models/reaction.js
@@ -0,0 +1,6 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema();
+
+module.exports = ReactionSchema;
---
+++ models/thought.js
@@ -0,0 +1,7 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
+const ReactionSchema = require("../models/reaction");
+
+const ThoughtSchema = new Schema();
+
+module.exports = Thought;
