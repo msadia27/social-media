854099f7f26ee1346317bf2a4b7baf6bdd3ad2ea
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-16 00:04:14 -0500

    finished models

--- models/thought.js
+++ models/thought.js
@@ -1,7 +1,35 @@
 const { Schema, model, Types } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 const ReactionSchema = require("../models/reaction");
 
-const ThoughtSchema = new Schema();
-
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Thought = model("Thought", ThoughtSchema);
 module.exports = Thought;
